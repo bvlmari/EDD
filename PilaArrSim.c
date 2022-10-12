91c0da4024afb0a5afafa1f4bfcbc1f5e2a19e6a
bvlmari <a1271949@uabc.edu.mx>
2022-10-12 10:57:47 -0700

    FInished PilaArrSim

--- PilaArrSim.c
+++ PilaArrSim.c
@@ -1,3 +1,6 @@
+//Mariano Perez Piña - 1271949
+//Pilas y arreglos simples
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -8,19 +11,71 @@ struct Pila{
 };
 
 struct Pila* crear(int capacidad){
-    struct Pila* pila = (struct Pila*)malloc(sizeof(struct Pila));//Reeserva del bloque de memoria
-    pila->capacidad = capacidad;//asignameos el tamaño de la pila
-    pila->top = -1; //inicializamos el valor en el tope de la pila
-    pila -> arreglo = (int*)malloc(pila->capacidad*sizeof(int)); // creamos un arreglo que almacena los datos
+    struct Pila* pila = (struct Pila*)malloc(sizeof(struct Pila));
+    pila->capacidad = capacidad;
+    pila->top = -1;
+    pila -> arreglo = (int*)malloc(pila->capacidad*sizeof(int));
     return pila;
 }
 
-int llena(struct Pila* pila){
-    return pila->top == pila->capacidad-1;
-}
+void push(struct Pila* pila, int elemento);
+int pop(struct Pila* pila);
+int numelementos(struct Pila* pila);
+int tope(struct Pila* pila);
+void imprimir(struct Pila* pila);
+int llena(struct Pila* pila);
+int vacia(struct Pila* pila);
 
-int vacia(struct Pila* pila){
-    return pila->top == -1;
+
+
+int main(){
+    int num, tam, op;
+
+    printf("\nBienvenido\nIngrese el tamaño deseado de la pila: ");
+    scanf("%d",&tam);
+    struct Pila* pila = crear(tam); //se crea con el tamaño deseado
+    
+    while(tam>0&&(op!=6)){
+        printf("\n\n[1]Añadir a la pila\n[2]Eliminar de la pila\n[3]Tamaño actual de la pila\n[4]Ver tope de la pila\n[5]Imprimir pila\n[6]Salir\nSeleccione una opcion --> ");
+        scanf("%d",&op);
+        switch(op){
+            case 1:
+                if(llena(pila))
+                    printf("\nLa pila esta llena");
+                else{
+                    printf("\nIngresa el numero que se quiere ingresar a la pila");
+                    scanf("%d",&num);
+                    push(pila, num);
+                }
+                break;
+
+            case 2:
+                pop(pila);
+                break;
+
+            case 3:
+                printf("\nLa pila tiene %d elementos\n", numelementos(pila));
+                break;
+
+            case 4:
+                printf("\nEl tope actual de la pila es %d ", tope(pila));
+                break;
+
+            case 5:
+                vacia(pila);
+
+                if(pila->top == -1)
+                    printf("\nLa pila esta vacia");
+                else
+                imprimir(pila);
+                break;
+                
+            default:
+                printf("\nOpcion ingresada incorrecta, intente de nuevo");
+        }
+    }
+    printf("\nSaliendo...");
+
 }
 
 void push(struct Pila* pila, int elemento){
@@ -35,10 +90,14 @@ int pop(struct Pila* pila){
     return pila->arreglo[pila->top--];
 }
 
-int peek(struct Pila* pila){
-    int val = pop(pila);
-    push(pila,val);
-    return val;
+int numelementos(struct Pila* pila){
+    return pila->top+1;
+}
+
+int tope(struct Pila* pila){
+	int top = pop(pila);
+	push(pila,top);
+	return top;
 }
 
 void imprimir(struct Pila* pila){
@@ -46,41 +105,10 @@ void imprimir(struct Pila* pila){
         printf("%i\n", pila->arreglo[i]);
 }
 
-int buscar(struct Pila* pila, int valor){
-    for(int i=0; i <= pila->top; i++)
-        if(pila->arreglo[i]==valor)
-            return i;
-    
-    return -1;
+int llena(struct Pila* pila){
+    return pila->top == pila->capacidad-1;
 }
 
-int numelementos(struct Pila* pila){
-    return pila->top+1;
-}
-
-int main(){
-    int num, tam, op;
-
-    printf("\nIngrese el tamaño deseado de la pila");
-    scanf("%d",&tam);
-    struct Pila* pila = crear(tam); //se crea con el tamaño deseado
-
-    printf("\nInsertar elemento a la pila\nRemover top de la pila\nSeleccione una opcion:")
-    scanf("%d",&op);
-
-    while(tam>0){
-        printf("\nIngresa el numero que se quiere ingresar a la pila");
-        scanf("%d",&num);
-        push(pila, num);
-        //pop(pila);
-        tam--;
-    }
-
-    pop(pila);
-
-    imprimir(pila);
-    printf("La pila tiene %i elementos\n", numelementos(pila));
-
-    if(llena(pila))
-        printf("\nLa pila esta llena");
+int vacia(struct Pila* pila){
+    return pila->top == -1;
 }
